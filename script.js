6d09e413b67494acf3bb56e76fd0a0accf6a431a
vamshi2112 <govindaramvamshi@gmail.com>
2026-06-17 08:45:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const student={
+	name:"john",
+	age:20,
+	city:"delhi"
+}
+function getKeys(student){
+	let result=object.keys(student);
+	return result;
+} console.log(getKeys(student));
